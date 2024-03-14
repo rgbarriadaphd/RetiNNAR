62023ac921ba3c08eb6bf890532897af91969c2d
rgbarriada <ruben.gbarriada@gmail.com>
2024-03-14 19:51:57 +0100

    first commint

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+# Author = ruben
+# Date: 14/3/24
+# Project: RetiNNAR
+# File: main.py
+
+Description: "Enter description here"
+"""
+
+if __name__ == '__main__':
+    print('This is the main script for RetiNNAR project')
